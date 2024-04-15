797729c3773004bdd5545397e13c0b6599c49899
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-04-15 19:37:30 -0300

    feat: add main file to start the program

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from clima import utilizar_rede_clima
+from condicoes_locais import utilizar_rede_condicoes_locais
+
+clima = utilizar_rede_clima()
+condicoes_locais = utilizar_rede_condicoes_locais(clima)
+
+
