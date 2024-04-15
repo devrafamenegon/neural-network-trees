5f7991d3904fce37ac01da0f80754cd76f682d1a
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-04-14 21:58:16 -0300

    feat: climate neural network training

--- src/clima.py
+++ src/clima.py
@@ -1,9 +1,14 @@
 import json
 from utils.coletar_resposta import coletar_resposta
+from utils.redimencionar_valores import redimencionar_valores
+from sklearn.neural_network import MLPClassifier
 
 # IMPORTAÇÃO DO SCRIPT COM AS OPÇÔES
 with open('src/scripts/clima.json', 'r', encoding='utf-8') as clima_json: 
   dados_clima = json.load(clima_json)
+
+with open('src/scripts/clima_resultados.json', 'r', encoding='utf-8') as clima_resultados_json: 
+  dados_clima_resultados = json.load(clima_resultados_json)
 
 categorias = {
   "temperatura": dados_clima["temperatura"],
@@ -25,3 +30,24 @@ respostas = {
   "tempestade": coletar_resposta("tempestade", categorias)
 }
 
+rede_clima = MLPClassifier(
+  solver='lbfgs',
+  activation='logistic', 
+  alpha=1e-8,
+  hidden_layer_sizes=(20, 20), 
+  random_state=1
+)
+
+x = []
+y = []
+
+climas = dados_clima_resultados["climas"]
+for clima in climas:
+  y.append(clima["nome"])
+  del clima["nome"]
+
+  x.append(redimencionar_valores(clima))
+
+rede_clima.fit(x, y)
+
+print(rede_clima.predict([redimencionar_valores(respostas)])[0])
