352bb7afaff6c0aba95f952b0838e5f71f9709c3
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-04-14 21:57:04 -0300

    fix: fix collect answers lenght check

--- src/utils/coletar_resposta.py
+++ src/utils/coletar_resposta.py
@@ -2,7 +2,7 @@ def coletar_resposta(categoria, categorias):
   while True:
     indice = 1
     opcoes = categorias[categoria]
-    numeroDeOpcoes = len(opcoes)
+    numeroDeOpcoes = len(opcoes) + 1
 
     for opcao in opcoes:
       print(indice, opcao)
@@ -10,7 +10,7 @@ def coletar_resposta(categoria, categorias):
     
     resposta = int(input("Escolha uma opção: "))
 
-    if (resposta >= numeroDeOpcoes | resposta <= numeroDeOpcoes):
+    if (resposta > 0 & resposta <= numeroDeOpcoes):
       return resposta
     
     print("Escolha uma das opções disponíveis")
