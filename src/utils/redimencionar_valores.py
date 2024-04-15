37ec2a98760906cc47a1a013aafa49f0f0d0cb76
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-04-15 18:10:15 -0300

    feat: remove unused function

--- src/utils/redimencionar_valores.py
+++ src/utils/redimencionar_valores.py
@@ -1,5 +1,2 @@
-def redimencionar_valores(valores, valor_maximo):
-  return list(map(lambda x: x / valor_maximo, [*valores.values()]))
-
 def redimencionar_valor_escala_1(valor_original, valor_maximo):
   return valor_original / valor_maximo
