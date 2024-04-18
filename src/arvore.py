11df32bc1210a1ebb6d541f67dc4b3fd07414c25
Rafael Menegon <dev.rafaelmenegon@gmail.com>
2024-04-18 17:30:37 -0300

    feat: add tree neural network

---
+++ src/arvore.py
@@ -0,0 +1,84 @@
+import json
+from utils.coletar_resposta import coletar_resposta
+from utils.redimencionar_valores import redimencionar_valor_escala_1
+from sklearn.neural_network import MLPClassifier
+
+# IMPORTAÇÃO DO SCRIPT COM AS OPÇÔES DE ENTRADA
+with open('src/scripts/arvore_opcoes_entrada.json', 'r', encoding='utf-8') as arvore_opcoes_entrada: 
+  opcoes_entrada = json.load(arvore_opcoes_entrada)
+
+# IMPORTAÇÃO DO SCRIPT COM AS ENTRADAS/SAIDAS TESTE
+with open('src/scripts/arvore_treinamentos.json', 'r', encoding='utf-8') as arvore_treinamentos: 
+  entradas_saidas = json.load(arvore_treinamentos)
+
+def treinar_rede():
+  # CRIAÇÃO DA REDE
+  rede_arvore = MLPClassifier(
+    solver='lbfgs',
+    activation='logistic', 
+    alpha=1e-8,
+    hidden_layer_sizes=(100, 100), 
+    random_state=1
+  )
+
+  # ENTRADAS TREINAMENTO
+  x = []
+
+  # SAÍDAS TREINAMENTO
+  y = []
+
+  # PARA CADA arvore
+  for arvore in entradas_saidas["treinamentos"]:
+    # DEFINA A SAIDA DA REDE COMO O NOME DO arvore
+    y.append(arvore["saida"])
+
+    # VARIAVEL QUE ARMAZENARA AS ENTRADAS REDIMENCIONADAS
+    entradas_teste_redimencionadas = []
+
+    for entrada in arvore["entradas"]:
+      valor = arvore["entradas"][entrada]
+      numeroDeOpcoes = len(opcoes_entrada[entrada])
+
+      # REDIMENCIONANDO AS ENTRADAS EM ESCALA ATÉ 1
+      entradas_teste_redimencionadas.append(
+        redimencionar_valor_escala_1(valor, numeroDeOpcoes)
+      )
+    
+    x.append(entradas_teste_redimencionadas)
+
+  for i in range(len(x)):
+    print(len(x[i]), y[i], x[i])
+
+  # TREINANDO A REDE
+  rede_arvore.fit(x, y)
+
+  return rede_arvore
+
+def utilizar_rede_arvore(clima, condicao_local, capacidade_manutencao):
+  # INSTANCIANDO UMA REDE TREINADA
+  rede_arvore = treinar_rede()
+
+  # COLETANDO ENTRADAS DO USUÁRIO
+  entradas_usuario = {
+    "clima": opcoes_entrada["clima"].index(clima),
+    "condicao_local": opcoes_entrada["condicao_local"].index(condicao_local),
+    "capacidade_manutencao": opcoes_entrada["capacidade_manutencao"].index(capacidade_manutencao),
+    "maturidade": coletar_resposta(opcoes_entrada["maturidade"]),
+    "tipo_de_copa": coletar_resposta(opcoes_entrada["tipo_de_copa"]),
+    "flores": coletar_resposta(opcoes_entrada["flores"]),
+    "frutos": coletar_resposta(opcoes_entrada["frutos"]),
+  }
+
+  entradas_usuario_redimencionadas = []
+
+  for entrada in entradas_usuario:
+    valor = entradas_usuario[entrada]
+    numeroDeOpcoes = len(opcoes_entrada[entrada])
+
+    entradas_usuario_redimencionadas.append(
+      redimencionar_valor_escala_1(valor, numeroDeOpcoes)
+    )
+
+  resultado = rede_arvore.predict([entradas_usuario_redimencionadas])
+  print("\n- ", resultado[0])
+  return resultado
--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from clima import utilizar_rede_clima
 from condicoes_locais import utilizar_rede_condicoes_locais
+from capacidade_manutencao import utilizar_rede_capacidade_manutencao
+from arvore import utilizar_rede_arvore
 
 print("\nEscolha Ideal de Árvores para Plantio 🌳🌿\n")
 
 clima = utilizar_rede_clima()
 condicoes_locais = utilizar_rede_condicoes_locais(clima)
-
-
-
+capacidade_manutencao = utilizar_rede_capacidade_manutencao(condicoes_locais)
+arvore = utilizar_rede_arvore(clima, condicoes_locais, capacidade_manutencao)
